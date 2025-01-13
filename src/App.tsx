b50eecc750f8f71bc7408fd2a0315daa5f2c5d11
emitex1 <emad.armoun@gmail.com>
2025-01-13 06:21:41 +0100

    read list of photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { fetchPhotos } from "./api";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [photos, setPhotos] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Flickr Analyzer</h1>
-      <h2>by Emad Armoun (EmiTex)</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    useEffect(() => {
+        const getPhotos = async () => {
+            const userId = import.meta.env.VITE_TARGET_USER_ID;
+            if (!userId) throw new Error("TARGET_USER_ID is not defined")
 
-export default App
+            const photos = await fetchPhotos(userId);
+            setPhotos(photos);
+        };
+        getPhotos();
+    }, []);
+
+    return (
+        <div>
+            <h1>My Flickr Dashboard</h1>
+            <div style={{ display: "flex", flexWrap: "wrap" }}>
+                {photos.map((photo: any) => (
+                    <div key={photo.id} style={{ margin: "10px" }}>
+                        <img
+                            src={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_q.jpg`}
+                            alt={photo.title}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default App;
