8fab8c90d77746916aec605c8b8f92dcad9ee9b0
emitex1 <emad.armoun@gmail.com>
2025-01-16 23:09:52 +0100

    fix the problem with axios

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -14,18 +14,29 @@ import axios from "axios";
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const fetchFlickrPhotos = functions.https.onRequest(async (req, res) => {
+export const fetchFlickrPhotos = functions.https.onRequest(async (req: any, res: any) => {
   logger.info("Before calling Flickr API!", {structuredData: true});
     try {
         const apiKey = functions.config().flickr.api_key;
-        const userId = req.query.userId || 'default_user_id';
+        if (!apiKey) throw new Error("Flickr API Key is not defined");
 
-        const flickrApiUrl = `https://www.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=${apiKey}&user_id=${userId}&format=json&nojsoncallback=1`;
-        const response = await axios.get(flickrApiUrl);
+        const userId = req.query.userId;
+        if (!userId) throw new Error("Target User ID is not defined");
 
-        res.status(200).json(response.data);
+        const isPublic = (req.query.isPublic || '').trim().toLowerCase() === 'true';
+        const flickrMethodName = isPublic ? 'getPublicPhotos' : 'getPhotos';
+        const flickrApiUrl = `https://www.flickr.com/services/rest/?method=flickr.people.${flickrMethodName}&api_key=${apiKey}&user_id=${userId}&format=json&nojsoncallback=1`;
+        
+        const response = await axios.get(flickrApiUrl, {
+          headers: {
+              'User-Agent': 'Mozilla/5.0',
+              'Accept': 'application/json',
+          }
+        });
+        const photos = response.data;
+        res.status(200).json(photos);
     } catch (error: any) {
         console.error('Error fetching Flickr photos:', error);
-        res.status(500).send('Error fetching Flickr photos: ' + error.message);
+        res.status(500).send('Error fetching Flickr photos:\n' + error.message);
     }
 });
