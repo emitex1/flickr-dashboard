301fd452220364ceaadfa4de4bd6a532ff71b6e2
emitex1 <emad.armoun@gmail.com>
2025-01-17 21:31:03 +0100

    read photos from firebase functions

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,35 +1,50 @@
 import { useEffect, useState } from "react";
-import { fetchPhotos } from "./api";
+import axios from "axios";
 
 const App = () => {
-    const [photos, setPhotos] = useState([]);
+	const [photos, setPhotos] = useState([]);
 
-    useEffect(() => {
-        const getPhotos = async () => {
-            const userId = import.meta.env.VITE_TARGET_USER_ID;
-            if (!userId) throw new Error("TARGET_USER_ID is not defined")
+	useEffect(() => {
+		const getPhotos = async () => {
+			try {
+				const userId = import.meta.env.VITE_TARGET_USER_ID;
+				if (!userId) throw new Error("TARGET_USER_ID is not defined");
+				console.log('user id is ' + userId);
 
-            const photos = await fetchPhotos(userId);
-            setPhotos(photos);
-        };
-        getPhotos();
-    }, []);
+				const response = await axios.get("https://fetchflickrphotos-ys2zam5ckq-uc.a.run.app?userId=" + userId, {
+						params: { tags: "nature" },
+				});
+				setPhotos(response.data.photos.photo);
+			} catch (error) {
+				console.error("Error fetching photos:", error);
+			}
+		};
+		getPhotos();
+	}, []);
 
-    return (
-        <div>
-            <h1>My Flickr Dashboard</h1>
-            <div style={{ display: "flex", flexWrap: "wrap" }}>
-                {photos.map((photo: {id: string; server: string; secret: string; title: string}) => (
-                    <div key={photo.id} style={{ margin: "10px" }}>
-                        <img
-                            src={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_q.jpg`}
-                            alt={photo.title}
-                        />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<h1>My Flickr Dashboard</h1>
+			<div style={{ display: "flex", flexWrap: "wrap" }}>
+				{photos.map(
+					(photo: {
+						id: string;
+						server: string;
+						secret: string;
+						title: string;
+					}) => (
+						<div key={photo.id} style={{ margin: "10px" }}>
+							<img
+								src={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_q.jpg`}
+								// src={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_w.jpg`}
+								alt={photo.title}
+							/>
+						</div>
+					)
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default App;
--- packages/frontend/src/api.ts
+++
@@ -1,9 +0,0 @@
-export const fetchPhotos = async (userId: string) => {
-    const FLICKR_API_KEY = import.meta.env.VITE_FLICKR_API_KEY;
-    if (!FLICKR_API_KEY) throw new Error("FLICKR_API_KEY is not defined");
-
-    const url = `https://www.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=${FLICKR_API_KEY}&user_id=${userId}&format=json&nojsoncallback=1`;
-    const response = await fetch(url);
-    const data = await response.json();
-    return data.photos.photo;
-};
