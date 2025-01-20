a4647d9946103779e28f7b945b664dad9d9a96c3
emitex1 <emad.armoun@gmail.com>
2025-01-20 22:18:28 +0100

    remove firebase comments

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,19 +1,7 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import * as functions from "firebase-functions";
 import * as logger from "firebase-functions/logger";
 import * as dotenvContent from "dotenv";
 import axios from "axios";
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
 
 async function getUserId(username: string, api_key: string) {
 	const url = "https://www.flickr.com/services/rest/";
