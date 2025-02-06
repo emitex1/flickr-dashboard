b250e9f1e2f2af8bcedb760eed8c3eea257b2041
emitex1 <emad.armoun@gmail.com>
2025-02-06 07:13:01 +0100

    fix the CORS problem in local env

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -6,7 +6,7 @@ import axios from "axios";
 const allowedOrigins = [
   'https://flickr-dashboard.web.app',
   'https://flickr-dashboard.firebaseapp.com',
-  'http://localhost:5173/'
+  'http://localhost:5173' //TODO: should be removed when deploying to production
 ];
 
 async function getUserId(username: string, api_key: string) {
