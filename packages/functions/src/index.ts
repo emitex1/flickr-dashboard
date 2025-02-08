9fe4dbadc7f2521d11fc4cbb87be02834d93e596
emitex1 <emad.armoun@gmail.com>
2025-02-08 11:15:52 +0100

    separate callFlickrAPI

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -10,52 +10,96 @@ const allowedOrigins = [
   'http://localhost:5174' //TODO: should be removed when deploying to production
 ];
 
-async function getUserId(username: string, api_key: string) {
+const callFlickrAPI = async (method: string, api_key: string, otherParams?: object) => {
 	const url = "https://www.flickr.com/services/rest/";
 	const params = {
-		method: "flickr.people.findByUsername",
+		method: method,
 		api_key: api_key,
-		username: username,
 		format: "json",
 		nojsoncallback: 1,
+		...otherParams,
 	};
 
-	try {
-		const response = await axios.get(url, {
-			params,
-			headers: {
-				"User-Agent": "Mozilla/5.0",
-				Accept: "application/json",
-			},
-		});
-		const data = response.data;
+	const response = await axios.get(url, {
+		params,
+		headers: {
+			"User-Agent": "Mozilla/5.0",
+			Accept: "application/json",
+		},
+	});
+	const data = response.data;
 
-		if (data.stat === "ok") {
-			const userId = data.user.id;
-			logger.info(`User ID for ${username}: ${userId}`);
-			return userId;
-		} else {
-			logger.error("Error:", data.message);
-		}
+	if (data.stat === "ok") {
+		return data;
+	} else {
+		logger.error("Error:", data.message);
+	}
+};
+
+async function getUserId(flickrUserName: string, api_key: string) {
+	try {
+		const data = await callFlickrAPI("flickr.people.findByUsername", api_key, {username: flickrUserName});
+		const userId = data.user.id;
+		logger.info(`User ID for ${flickrUserName}: ${userId}`);
+		return userId;
 	} catch (error: any) {
 		logger.error("API request failed:", error.message);
 		throw new Error("API request failed:" + error.message);
 	}
 }
 
+const checkCORS = (req: any, res: any) => {
+	const currentOrigin = req.headers.origin;  
+	if (allowedOrigins.includes(currentOrigin)) {
+		res.set('Access-Control-Allow-Origin', currentOrigin);
+	}
+	res.set("Access-Control-Allow-Methods", "GET, OPTIONS");
+	res.set("Access-Control-Allow-Headers", "Content-Type");
+
+	if (req.method === "OPTIONS") {
+		res.status(204).send("");
+		return;
+	}
+}
+
+export const checkFlickrUserName = functions.https.onRequest(
+  async (req: any, res: any) => {
+		checkCORS(req, res);
+
+		logger.info("Checking Flickr User Name.");
+
+		try {
+			dotenvContent.config();
+			const apiKey = process.env.FLICKR_API_KEY;
+			if (!apiKey) throw new Error("Flickr API Key is not defined");
+			logger.info("Flickr API Key is available.");
+
+			const flickrUserName = req.query.userName || '';
+			if (!flickrUserName) throw new Error("A user name should be provided.");
+			logger.info(`Target User Name: ${flickrUserName}`);
+
+			const data = await callFlickrAPI("flickr.people.findByUsername", apiKey, { username: flickrUserName });
+
+			if (data.stat === "ok") {
+				const userId = data.user.id;
+				logger.info(`User ID for ${flickrUserName}: ${userId}`);
+				res.status(200).json({
+					flickrUserId: userId
+				});
+			} else {
+				logger.error("Error:", data.message);
+				res.status(404);
+			}
+
+		} catch (error: any) {
+			logger.error("Error fetching Flickr User:", error);
+			res.status(500).send("Error fetching Flickr User:\n" + error.message);
+		}
+	});
+
 export const fetchFlickrPhotos = functions.https.onRequest(
   async (req: any, res: any) => {
-    const currentOrigin = req.headers.origin;  
-    if (allowedOrigins.includes(currentOrigin)) {
-      res.set('Access-Control-Allow-Origin', currentOrigin);
-    }
-		res.set("Access-Control-Allow-Methods", "GET, OPTIONS");
-		res.set("Access-Control-Allow-Headers", "Content-Type");
-
-		if (req.method === "OPTIONS") {
-			res.status(204).send("");
-			return;
-		}
+    checkCORS(req, res);
 
 		logger.info("Fetching the photos is started.");
 		try {
@@ -72,19 +116,18 @@ export const fetchFlickrPhotos = functions.https.onRequest(
 			const isPublic =
 				(req.query.isPublic || "").trim().toLowerCase() === "true";
 			const flickrMethodName = isPublic ? "getPublicPhotos" : "getPhotos";
-			const flickrApiUrl = `https://www.flickr.com/services/rest/?method=flickr.people.${flickrMethodName}&api_key=${apiKey}&user_id=${userId}&format=json&nojsoncallback=1`;
 
-			const response = await axios.get(flickrApiUrl, {
-				headers: {
-					"User-Agent": "Mozilla/5.0",
-					Accept: "application/json",
-				},
+			const result = await callFlickrAPI("flickr.people." + flickrMethodName, apiKey, {
+				user_id: userId
 			});
-			logger.info("Photos are fetched from Flickr API.");
-			const photos = response.data;
-			logger.info(`${photos.photos.total} photos are fetched.`);
 
-			res.status(200).json(photos);
+      // if (photos.stat === "fail") {
+      //   logger.error("Error:", photos.message);
+      //   res.status(404);
+      // }
+			logger.info(`${result.photos.total} photos are fetched.`);
+
+			res.status(200).json(result);
 		} catch (error: any) {
 			logger.error("Error fetching Flickr photos:", error);
 			res.status(500).send("Error fetching Flickr photos:\n" + error.message);
