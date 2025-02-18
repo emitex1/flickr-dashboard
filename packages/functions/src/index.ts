6136047d01a4f12d34f6bd3035c37c15d446de61
emitex1 <emad.armoun@gmail.com>
2025-02-18 09:22:52 +0100

    separate flickr util codes

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,55 +1,12 @@
 import admin from "firebase-admin";
 import * as functions from "firebase-functions";
 import * as logger from "firebase-functions/logger";
-import * as dotenvContent from "dotenv";
-import axios from "axios";
 import { failResult, successResult } from "./util/generalResult";
 import { checkAuthorization, checkCORS } from "./util/webUtils";
+import { callFlickrAPI, getFlickrAPIKey } from "./util/flickrUtils";
 
 admin.initializeApp();
 const db = admin.firestore();
-
-const getFlickrAPIKey = () => {
-	dotenvContent.config();
-	const apiKey = process.env.FLICKR_API_KEY;
-	logger.info("Flickr API Key is available.");
-	if (!apiKey) throw new Error("Flickr API Key is not defined");
-	return apiKey;
-};
-
-const callFlickrAPI = async (
-	method: string,
-	api_key: string,
-	otherParams?: object
-) => {
-	const url = "https://www.flickr.com/services/rest/";
-	const params = {
-		method: method,
-		api_key: api_key,
-		format: "json",
-		nojsoncallback: 1,
-		...otherParams,
-	};
-
-	const response = await axios.get(url, {
-		params,
-		headers: {
-			"User-Agent": "Mozilla/5.0",
-			Accept: "application/json",
-		},
-	});
-	const data = response.data;
-
-	if (data.stat === "ok") {
-		return data;
-	} else {
-		logger.error("Error:", data.message);
-	}
-	// if (photos.stat === "fail") {
-	//   logger.error("Error:", photos.message);
-	//   res.status(404);
-	// }
-};
 
 const getUserId = async (flickrUserName: string, api_key: string) => {
 	try {
