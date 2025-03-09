8d16369370644182c4272b11e18ea3d4668d4c93
emitex1 <emad.armoun@gmail.com>
2025-03-09 05:43:30 +0100

    fix the problem with profile page

--- packages/frontend/src/components/Headers/UserHeader.tsx
+++ packages/frontend/src/components/Headers/UserHeader.tsx
@@ -39,12 +39,12 @@ const UserHeader: React.FC<Props> = ({ user }) => {
         }}
       >
         {/* Mask */}
-        <span className="mask bg-gradient-default opacity-8" />
+        <span className="mask bg-gradient-default opacity-2" />
         {/* Header container */}
         <Container className="d-flex align-items-center" fluid>
           <Row>
             <Col lg="7" md="10">
-              <h1 className="display-2 text-white">Hello {user.name}</h1>
+              <h1 className="display-2 text-white">Hello {user?.name}</h1>
               <p className="text-white mt-0 mb-5">
                 This is your profile page. You can see the progress you've made
                 with your work and manage your projects or assigned tasks
