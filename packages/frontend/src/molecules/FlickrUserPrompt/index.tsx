bec9098e63006ecb193ddcf8bc71fd0057b55e94
emitex1 <emad.armoun@gmail.com>
2025-03-09 07:37:44 +0100

    make the settings page responsive

--- packages/frontend/src/molecules/FlickrUserPrompt/index.tsx
+++ packages/frontend/src/molecules/FlickrUserPrompt/index.tsx
@@ -37,7 +37,7 @@ export const FlickrUserPrompt: React.FC = () => {
 			<Form>
 				<FormGroup>
 					<Row>
-						<Col xl="4" lg="6" md="8" xs="8">
+						<Col xl="4" lg="6" md="8" sm="8" xs="12">
 							<Input
 								id="FlickrUserId"
 								type="text"
@@ -48,9 +48,9 @@ export const FlickrUserPrompt: React.FC = () => {
 							/>
 						</Col>
 
-						<Col xl="2" lg="3" md="4" xs="4">
+						<Col xl="2" lg="3" md="4" sm="4" xs="12" className="mt-3 mt-sm-0 px-3 py-0 m-0">
 							<Button
-								className="btn-icon btn-3"
+								className="btn-icon btn-3 w-100"
 								color="primary"
 								type="button"
 								onClick={saveInfo}
