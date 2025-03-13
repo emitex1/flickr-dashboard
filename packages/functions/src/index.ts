9d95f15e9fc799c42e0aad2607b2f43bb7d1453e
emitex1 <emad.armoun@gmail.com>
2025-03-13 06:28:05 +0100

    add function for the fetching recent photos

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -154,11 +154,53 @@ export const fetchFlickrPhotos = functions.https.onRequest(
 	}
 );
 
+export const fetchRecentFlickrPhotos = functions.https.onRequest(
+	async (req: any, res: any) => {
+		const logPrefix = "[FetchRecentPhotos]";
+		const log = (message: string, ...params: unknown[]) => logger.info(logPrefix, message, ...params);
+
+		log("fetchRecentFlickrPhotos() is called.");
+
+		checkCORS(req, res);
+
+		try {
+			const authResult = await checkAuthorization(req, admin);
+			if (!authResult.isDone) {
+				logger.error(logPrefix, "Error:", authResult.message);
+				res.status(authResult.status).json({ error: authResult.message });
+				return;
+			}
+			const currentFirebaseUserId = authResult.data as string;
+			const flickrUserId = await readCurrentUserFlickrId(currentFirebaseUserId);
+			log(`Flickr User ID: ${flickrUserId}`);
+
+			const todayMorning = new Date();
+			todayMorning.setHours(5, 0, 0, 0);
+			log("Today Morning:", todayMorning);
+			const result = await callFlickrAPI("flickr.photos.search", {
+				user_id: flickrUserId,
+				min_upload_date: todayMorning,
+			});
+			log(`${result.photos.total} photos are fetched.`);
+
+			res.status(200).json(result);
+		} catch (error: unknown) {
+			logger.error(logPrefix, "Error fetching recent Flickr photos:", error);
+			res
+				.status(500)
+				.send(
+					"Error fetching recent Flickr photos:\n" +
+						(error as { message: string }).message
+				);
+		}
+	}
+);
+
 export const saveFlickrPhotos = functionsV2.onSchedule(
 	"every day 05:00",
 	async () => {
 		const logPrefix = "[UpdatePhotos]";
-		const log = (message: string) => logger.info(logPrefix, message);
+		const log = (message: string, ...params: unknown[]) => logger.info(logPrefix, message, ...params);
 
 		log("Cron Job Started: Fetching Flickr Photos...");
 
